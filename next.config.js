1efea34152b15c1af4a9f91a2f10d7884d6345e4
Usman-Amjad <uamjad06@gmail.com>
2022-07-25 04:37:24 +0500

    changes in next config js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     domains: [
       'images.unsplash.com',
